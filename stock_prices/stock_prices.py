2237ec805815d3712b6c8e73fa2266d09fdae81d
Joel Perez <joelness03@gmail.com>
2019-10-09 13:58:00 -0600

    finished stock_prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -6,15 +6,25 @@ import argparse
 def find_max_profit(prices):
     current_min_price_index = 0
     current_max_profit = 0
+    # For loop to find index of lowest price
     for index, price in enumerate(prices):
+        # Index is initialized at 0
         if index > 0:
+            # If the price is less than the price at index 0
             if price <= prices[current_min_price_index]:
+                # Update current_min_price_index
                 current_min_price_index = index
+                # Resolves edge case of lowest number at the end of prices
                 if index != len(prices)-1:
+                    # Loop through everything after current_min_price_index and find new current_max_profit
                     for i in range(current_min_price_index+1, len(prices)-1):
                         if prices[i]-prices[current_min_price_index] > current_max_profit:
                             current_max_profit = prices[i] - \
                                 prices[current_min_price_index]
+        else:
+          # initialize current_max_profit based on the difference between first and second prices
+          # Resolves negative profit issues relating to initializing current_max_profit at 0
+            current_max_profit = prices[1] - prices[0]
     return current_max_profit
 
 
