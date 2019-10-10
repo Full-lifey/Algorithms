a41b5d4a2fe3720e3b7528852e421ef60dfbbf5a
Joel Perez <joelness03@gmail.com>
2019-10-10 16:32:02 -0600

    eating cookies answer provided, rps finished

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,26 +5,24 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive
 # recursive solution
-count = 0
 
 
 def eating_cookies(n, cache=None):
-    print(n)
-    global count
-    # Base case
-    if n <= 0:
-        return n
+    # Base cases
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
     elif n == 1:
-        return n
-    count += 1
-    # Start by cookie monster taking three cookies if n > 3
-    if n-3 > 0:
-        return eating_cookies(n-3)
-    if n-2 > 0:
-        return eating_cookies(n-2)
-    if n-1 > 0:
-        return eating_cookies(n-1)
-    return count
+        return 1
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            cache = {i: 0 for i in range(n+1)}
+        cache[n] = eating_cookies(
+            n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
 
 
 if __name__ == "__main__":
@@ -34,17 +32,3 @@ if __name__ == "__main__":
             ways=eating_cookies(num_cookies), n=num_cookies))
     else:
         print('Usage: eating_cookies.py [num_cookies]')
-
-
-cache = {}
-
-
-def dynamic_fib(n):
-    global cache
-    if n < 2:
-        return n
-    elif n in cache:
-        return cache[n]
-    else:
-        cache[n] = dynamic_fib(n - 1) + dynamic_fib(n - 2)
-        return cache[n]
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,36 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    # initialize an array with possible options
+    rps = ['rock', 'paper', 'scissors']
+    # for play in rps:
+    #   for play2 in rps:
+    #     result.append([play, play2])
+
+    '''
+  For n=2
+  loop over rps
+    for every item in rps
+      [rps[0], rps2[0]], [rps[0], [rps[1]]]
+  '''
+    results = []
+
+    def loop(n, previous_plays=[]):
+        # base case
+        if n == 0:
+            return results.append(previous_plays)
+        else:
+            for play in rps:
+                loop(n-1, [*previous_plays, play])
+    loop(n)
+    return results
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
